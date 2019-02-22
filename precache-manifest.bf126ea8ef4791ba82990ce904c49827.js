4e57fede481b10f3df645d6a1354d8df0d9e1165
nenadV91 <nenadwebdevelop@gmail.com>
2019-02-22 20:01:49 +0100

    Initial commit

---
+++ precache-manifest.bf126ea8ef4791ba82990ce904c49827.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "faeb3ab4c0b55c28f839",
+    "url": "./static/js/main.faeb3ab4.chunk.js"
+  },
+  {
+    "revision": "ffb983b215ddcdb2d90d",
+    "url": "./static/js/2.ffb983b2.chunk.js"
+  },
+  {
+    "revision": "faeb3ab4c0b55c28f839",
+    "url": "./static/css/main.6ff44d82.chunk.css"
+  },
+  {
+    "revision": "ffb983b215ddcdb2d90d",
+    "url": "./static/css/2.8bc83570.chunk.css"
+  },
+  {
+    "revision": "b094115ba77d103f49e0b5cfa4be2234",
+    "url": "./index.html"
+  }
+];
